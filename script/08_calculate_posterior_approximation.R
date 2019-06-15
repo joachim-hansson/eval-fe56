8627eb41ed2df3570738412060d1d03d94cae03b
gschnabel <40870991+gschnabel@users.noreply.github.com>
2019-06-15 20:13:47 +0000

    added step 8 to calculuate posterior approximation

---
+++ script/08_calculate_posterior_approximation.R
@@ -0,0 +1,119 @@
+#
+# DESCRIPTION OF STEP
+#
+# Approximate the posterior distribution
+# by constructing a second order Taylor
+# polynomial at the parameter set 
+# found by the Levenberg-Marquardt algorithm.
+#
+
+source("config.R")
+
+#################################################
+#       SCRIPT PARAMETERS
+##################################################
+
+scriptnr <- 8L
+overwrite <- FALSE
+
+##################################################
+#       OUTPUT FROM PREVIOUS STEPS
+##################################################
+
+extNeedsDt <- read_object(2, "extNeedsDt")
+optParamDt <- read_object(7, "optParamDt")
+Sexp <- read_object(7, "Sexp")
+mask <- read_object(7, "mask")
+refPar <- read_object(7, "refPar")
+P0 <- read_object(7, "P0")
+yexp <- read_object(7, "yexp")
+D <- read_object(7, "D")
+S0 <- read_object(7, "S0")
+X <- read_object(7, "X")
+optRes <- read_object(7, "optRes")
+
+##################################################
+#       START OF SCRIPT
+##################################################
+
+# define objects to be returned
+outputObjectNames <- c("variationMat", "variationMatRes", 
+                       "finalPars", "finalParCovmat")
+check_output_objects(scriptnr, outputObjectNames)
+
+# see step 07_tune_talyspars.R for more explanation
+# about setting up the talys handler
+talysHnds <- createTalysHandlers()
+talys <- talysHnds$talysOptHnd
+talys$setPars(optParamDt)
+talys$setParTrafo(paramTrafo$fun, paramTrafo$jac)
+talys$setNeeds(extNeedsDt)
+talys$setSexp(Sexp)
+talys$setMask(mask)
+talys$setEps(0.01)
+
+# construct an object that contains functions to
+# evaluate the log posterior density (up to a constant)
+# and its gradient. It makes use of the talys handler
+# and takes in addition the parameters defining the
+# posterior distribution.
+logPost <- setupLogPost(refPar, P0, yexp, D, S0, X, talys)
+
+# define the variations needed to calculate the diagonal
+# elements of the Hessian matrix
+workEps <- 0.02
+optPars <- optRes$par
+
+idx1 <- seq_along(optPars) 
+idx2 <- 1+seq_along(optPars)
+idx3 <- 1+length(optPars)+seq_along(optPars)
+
+variationMat <- matrix(optPars, nrow = length(optPars), ncol = 1+length(optPars)*2)
+variationMat[cbind(idx1,idx2)] <- variationMat[cbind(idx1,idx2)] - workEps
+variationMat[cbind(idx1,idx3)] <- variationMat[cbind(idx1,idx3)] + workEps
+
+# perform the calculations using the varied parameter sets
+variationMatRes <- talys$fun(variationMat)
+logPostVals <- logPost$fun(variationMat, variationMatRes)
+
+# calculate the diagonal of the Hessian matrix
+# by evaluating the 2nd derivative using finite differences
+fp <- (logPostVals[idx3] - logPostVals[idx2]) / (2*workEps)
+fpp <- (logPostVals[idx3] - 2 * logPostVals[1] + logPostVals[idx2]) / workEps^2
+finalParCovmat <- 1/fpp
+finalPars <- optPars
+
+# IMPORTANT REMARK: 
+# If Levenberg-Marquardt did not converge sufficiently 
+# to a (local) posterior maximum, some values in 
+# finalParCovmat may be negative. Such a result does
+# not correspond to a proper covariance matrix.
+
+# save the result also in a more human-readable way
+# as a table also containing the parameter names
+finalParamDt <- copy(optParamDt)
+setkey(finalParamDt, IDX)
+finalParamDt[ADJUSTABLE == TRUE, POSTVAL := paramTrafo$fun(optPars)]
+# IMPORTANT NOTE: POSTUNC is still with respect to transformed parameters
+finalParamDt[ADJUSTABLE == TRUE, POSTUNC := sqrt(finalParCovmat)]
+
+# sanity check:
+# make sure that the results obtained by the two different
+# talys handlers talysOptHnd and talysHnd coincide.
+# This is indication that we have indeed used the correct parameters
+# in the optimization routine. In principle, it is still possible
+# that the matrix Sexp is not correctly specified. This can be
+# checked by looking at the plots comparing predictions and
+# experimental data.
+# finalParamDt[!is.na(POSTVAL), PARVAL := as.list(POSTVAL)]
+# myInp <- convertToInput(finalParamDt, extNeedsDt)
+# res1 <- talysHnds$talysOptHnd$fun(finalPars)
+# runObj <- talysHnds$talysHnd$run(myInp$inputs, myInp$outspecs)
+# talysHnds$talysHnd$isRunning(runObj)
+# rawRes2 <- talysHnds$talysHnd$result(runObj)
+# res2 <- as.vector(Sexp %*% rawRes2[[1]]$result$V1)
+# stopifnot(all(res1 == res2))
+# sanity check end
+
+# save the needed files for reference
+save_output_objects(scriptnr, outputObjectNames, overwrite)
