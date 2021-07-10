02a4612bbac067701d12585a05c21c129cc312b9
Joachim Hansson <joachim.hansson@protonmail.com>
2021-07-10 18:03:12 +0000

    Changed tolerance in step 8: 1e-10 to 1e-9. Cov matrix returns as not symmetric if tolerance is too low. Could be numeric issues.

--- script/08_calculate_posterior_approximation_with_defect.R
+++ script/08_calculate_posterior_approximation_with_defect.R
@@ -135,7 +135,7 @@ finalParCovmat <- (-1) * solve(H)
 
 # if LM algorithm did not sufficiently converge
 # in step 07, the covariance matrix would not be well defined
-stopifnot(isSymmetric(finalParCovmat, tol=1e-10))
+stopifnot(isSymmetric(finalParCovmat, tol=1e-9))
 finalParCovmat <- (finalParCovmat + t(finalParCovmat)) / 2
 stopifnot(all(eigen(finalParCovmat)$values >= 0))
 
